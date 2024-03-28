0fa12dbfd9a3742e19eb6edf55d3fd60900b8123
Rajeev Ranjan <r.s.ranjan505@gmail.com>
2024-03-28 09:00:29 +0530

    getb up

--- resources/views/landing/components/hero-section.blade.php
+++ resources/views/landing/components/hero-section.blade.php
@@ -150,21 +150,15 @@
                         <h2 class="display-1 text-white mb-md-1 animated zoomIn">For Long Term <strong>Success</strong></h2>
                         <p class="mb-md-3">Transform Your Business With Our Strategic Consulting Services</p>
                         <div class="form-rounded input-form-bg w-50" style="max-width: 300px;">
-                            <form action="">
-                                <div class="input-group ">
-                                    <div class="input-group-prepend">
-                                        <span class="input-start-icon"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-                                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-                                          </svg></span>
-                                      </div>
-                                    <input type="text" class="form-control border-0 p-3 input-form-bg shadow-none"  placeholder="Your Email" style="max-width: 300px;">
-                                    <button class="btn btn-primary rounded-circle btn-sm btn-arrow-icon" >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
-                                          </svg>
-                                    </button>
-                                </div>
-                            </form>
+
+                            <button class="btn btn-primary btn-lg bg shadow-none btn-book w-100" >Book A Demo
+
+                            </button>
+                            {{-- <button class="  rounded-circle" style="border: 1px solid;
+
+                                background-color: #fff;">
+                                    <i class="bi bi-arrow-right-short text-primary"></i>
+                                </button> --}}
                         </div>
                     </div>
                 </div>
--- resources/views/landing/components/service-banner.blade.php
+++ resources/views/landing/components/service-banner.blade.php
@@ -8,18 +8,14 @@
                     <h2 class="text-primary mb-md-1"><span class="text-white mb-md-4">Visit our IVT Platform to learn how our solutions can help you</span> Work Better and Smarter.</h2>
 
 
-                    <button class="btn btn-primary btn-lg bg shadow-none btn-book pl-4 py-2" >BOOK A DEMO
+                    <button class="btn btn-primary btn-lg bg shadow-none btn-book pl-4 py-2" >Book A Demo
                         <span class="ml-4  rounded-circle" style="border: 1px solid;
                         padding: 5px;
                         margion-right:-10px;
                         background-color: #fff;">
                             <i class="bi bi-arrow-right-short text-primary"></i>
                         </span>
-
-
                     </button>
-
-
                 </div>
             </div>
         </div>
