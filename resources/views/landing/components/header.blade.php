1c91d92a05fab632d55aea618dc8ac2721bbc898
Jas Raj Singh <87642495+jasraj25@users.noreply.github.com>
2024-04-10 18:19:01 +0530

    navbar and footer update

--- resources/views/landing/components/header.blade.php
+++ resources/views/landing/components/header.blade.php
@@ -11,7 +11,237 @@
             </div>
         </div>
     </div>
+
+
+
     <div class="container-fluid position-relative p-0">
+    <nav class="navbar navbar-expand-lg navbar-dark px-5 py-4 py-lg-0">
+        <a href="{{route('home')}}" class="navbar-brand p-0 ml-4 wh-img">
+            <img class="" src="{{ asset('assets/img/logo-wh.png')}}" width="64px"/>
+        </a>
+        <a href="{{route('home')}}" class="navbar-brand p-0 ml-4 dk-img">
+            <img class="" src="{{ asset('assets/img/logo.png')}}" width="64px"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span>
+        </button>
+        <div class="collapse navbar-collapse aligns-content-center justify-content-center pt-2" id="main_nav">
+            <div class="navbar-nav ms-auto py-0">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item ">
+                        <a href="{{route('home')}}" class="nav-item nav-link {{ Request::is('/') ? 'active' : ''}} ">Home</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a href="{{route('about')}}" class="nav-link {{ Request::is('about-us') ? 'active' : ''}}" >Company</a>
+                    </li>
+                    <!-- <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle {{ str_contains(Request::url(),'services/') ? 'active' : ''}}" id="dropdown1" data-toggle="dropdown" aria-haspopup="true"
+                           aria-expanded="false">Services</a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown1">
+                            <li class="dropdown-item"><a href="{{ route('services.staffing')}}">Staffing Services</a></li>
+                            <li class="dropdown-item"><a href="{{ route('services.consulting')}}">Consulting Services</a></li>
+                        </ul>
+                    </li> -->
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle {{ str_contains(Request::url(),'solutions/') ? 'active' : ''}}" id="dropdown2" data-toggle="dropdown" aria-haspopup="true"
+                           aria-expanded="false">Services</a>
+                        <ul class="dropdown-menu mega-dropdown" aria-labelledby="dropdown2">
+                            <div class="row">
+
+
+                             <!-- jas test -->
+
+<div class="c-container">
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/r04.png')}}" alt="">
+                            <div class="card-content">
+                                <p class="card-head">Cloud Transformation</p>
+                                <div class="block"></div>
+                                <p>Streamlining and transforming businesses into cloud-based solutions for enhanced scalability and efficiency.
+                                    </p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/r05.png')}}" alt="">
+                            <div class="card-content">
+                            <p class="card-head">IT Lifecycle Management</p>
+                            <div class="block"></div>
+                                            <p>Comprehensive management of IT infrastructure from planning to retirement, ensuring optimal performance throughout.</p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/r06.png')}}" alt="">
+                            <div class="card-content">
+                                <p class="card-head">IT Security Audit Services</p>
+                                <div class="block"></div>
+                                            <p>Performing thorough assessments to identify vulnerabilities and fortify digital assets against cyber threats.
+                                </p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/r01.png')}}" alt="">
+                            <div class="card-content">
+                                <p class="card-head">Permanent Hiring</p>
+                                <div class="block"></div>
+                                <p>Seamless recruitment processes to secure permanent talent perfectly suited to organizational needs.
+                                    </p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/r02.png')}}" alt="">
+                            <div class="card-content">
+                            <p class="card-head">Contract Consultants</p>
+                            <div class="block"></div>
+                                            <p>Get full access of top-tier IT expertise on a flexible basis to support project-specific requirements.
+</p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/r03.png')}}" alt="">
+                            <div class="card-content">
+                                <p class="card-head">Recruitment Outsourcing</p>
+                                <div class="block"></div>
+                                            <p>Outsourcing of the recruitment process to expert professionals for efficient and effective candidate selection.
+                                </p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+
+                            </div>
+                        </ul>
+                    </li>
+
+                   
+
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle {{ str_contains(Request::url(),'solutions/') ? 'active' : ''}}" id="dropdown2" data-toggle="dropdown" aria-haspopup="true"
+                           aria-expanded="false">Solutions</a>
+                        <ul class="dropdown-menu mega-dropdown" aria-labelledby="dropdown2">
+                            <div class="row">
+
+
+                             <!-- jas test -->
+
+<div class="c-container">
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/c 01.png')}}" alt="">
+                            <div class="card-content">
+                                <p class="card-head">IT Infrastructure Platform</p>
+                                <div class="block"></div>
+                                <p>Build a resilient foundation tailored to your needs, ensuring seamless operations and scalability.
+                                    </p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/c 02.png')}}" alt="">
+                            <div class="card-content">
+                            <p class="card-head">IT Security Solutions</p>
+                            <div class="block"></div>
+                                            <p>Safeguard your digital assets with cutting-edge defenses, protecting against evolving cyber threats and vulnerabilities.</p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/c 03.png')}}" alt="">
+                            <div class="card-content">
+                                <p class="card-head">Cloud Solutions</p>
+                                <div class="block"></div>
+                                            <p>Unlock agility & efficiency with customized cloud solutions, enabling scalable & flexible IT resources for your business.
+                                </p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/c 04.png')}}" alt="">
+                            <div class="card-content">
+                                <p class="card-head">Collaboration & Digital Workplace</p>
+                                <div class="block"></div>
+                                <p>Foster collaboration and productivity in the digital realm, optimizing workflows and communication channels.
+                                    </p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/c 05.png')}}" alt="">
+                            <div class="card-content">
+                            <p class="card-head">Enterprise Application Modernization</p>
+                            <div class="block"></div>
+                                            <p>Revamp legacy systems for enhanced performance and functionality, driving innovation and agility across your organization.</p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+
+                        <div class="card-test">
+                            <img src="{{ asset('assets/img/c 06.png')}}" alt="">
+                            <div class="card-content">
+                                <p class="card-head">Consulting & Digital Transformation</p>
+                                <div class="block"></div>
+                                            <p>Expert guidance for digital transformation, leveraging technology to stay ahead in a competitive landscape.
+                                </p>
+                                  <a href="" ><p class="know-more">Know More &rarr;</p></a>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+
+                            </div>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('blogs')}}" class="nav-item nav-link {{ Request::is('blogs') || str_contains(Request::url(),'blogs') ? 'active' : ''}}">Blogs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('career')}}" class="nav-item nav-link {{ Request::is('careers') || str_contains(Request::url(),'careers') ? 'active' : ''}}">Career</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('contact')}}" class="nav-item nav-link {{ Request::is('contact-us') ? 'active' : ''}}">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="text-end text-lg-end mr-4 social-icons">
+            <div class="d-inline-flex align-items-center" style="height: 45px; margin-left:4px;">
+                <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2 mr-2 box-shadow" href=""><i class="fab fa-linkedin-in fw-normal" style="color: #0077b5 ;"></i></a>
+                <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2 box-shadow" href=""><svg class="ml-2 mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 912 912"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg></a>
+            </div>
+        </div>
+    </nav> 
+</div>
+
+
+
+
+    <!-- <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-dark px-5 py-4 py-lg-0">
             <a href="{{route('home')}}" class="navbar-brand p-0 ml-4 wh-img">
                 <img class="" src="{{ asset('assets/img/logo-wh.png')}}" width="64px"/>
@@ -94,7 +324,7 @@
                     <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2 box-shadow" href=""><svg class="ml-2 mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 912 912"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg></a>
                 </div>
             </div>
-        </nav>
+        </nav> -->
 
         @if (Request::is('/'))
             @include('landing.components.hero-section')
@@ -145,5 +375,5 @@
 
 
 
-    </div>
+    <!-- </div> -->
     <!-- Navbar & Carousel End -->
--- resources/views/landing/pages/case-study.blade.php
+++ resources/views/landing/pages/case-study.blade.php
@@ -83,7 +83,7 @@
                 <h5 class="mb-0 pb-1"><strong>Challenge:</strong></h5>
                 <p class="mb-4 text-justify">SmartCityTech Solutions pioneers innovative IT services, specializing in the development of integrated command and control solutions for smart cities. With a focus on dynamic data analysis and security, they empower urban environments to make efficient, informed, and coordinated decisions, driving forward the evolution of modern city infrastructure.</p>
 
-                <a href="{{ route('case-study-2')}}" class="btn btn-dark rounded-pill py-3 px-5 wow zoomIn" data-wow-delay="0.9s">Know More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                <a href="{{ route('case-study-pragraj')}}" class="btn btn-dark rounded-pill py-3 px-5 wow zoomIn" data-wow-delay="0.9s">Know More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
                   </svg></a>
             </div>
